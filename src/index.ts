0766c58ff663f9e0b73b65ca278e432619bc61f5
HashWarlock <hashwarlock@protonmail.com>
2023-09-06 18:16:20 -0500

    Update function to use batch http request and use result to propagate to Telegram group

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import "@phala/pink-env";
 
-const odds_http_endpoint = "https://api.the-odds-api.com/v4/sports/";
-const kvdb_http_endpoint = "https://kvdb.io/{}/";
-const tg_bot_http_endpoint = "https://api.telegram.org/bot{}/sendMessage?chat_id=-1001986190934&text={}";
-
 function stringToHex(str: string): string {
   var hex = "";
   for (var i = 0; i < str.length; i++) {
@@ -13,6 +9,11 @@ function stringToHex(str: string): string {
 }
 
 export default function(args: any[]) {
+  const sportName = 'baseball_mlb'
+  const odds_http_endpoint = `https://api.the-odds-api.com/v4/sports/${sportName}/scores/?apiKey={}`;
+  const kvdb_http_endpoint = `https://kvdb.io/{}`;
+  const tg_bot_http_endpoint = `https://api.telegram.org/bot{}/sendMessage?chat_id=-1001986190934&text=`;
+
   let headers = {
     "Content-Type": "application/json",
     "User-Agent": "phat-contract",
@@ -20,57 +21,39 @@ export default function(args: any[]) {
   let headers2 = {
     "Content-Type": "application/json",
     "User-Agent": "phat-contract",
-    "Authorization": "Bearer {}",
+    "Authorization": `Bearer {}`,
   };
-  const profileId = '0x01'
-  let query = JSON.stringify({
-    query: `query Profile {
-            profile(request: { profileId: \"${profileId}\" }) {
-                stats {
-                    totalFollowers
-                    totalFollowing
-                    totalPosts
-                    totalComments
-                    totalMirrors
-                    totalPublications
-                    totalCollects
-                }
-            }
-        }`,
-  });
-  const body = stringToHex(query);
-  console.log(`${body}`);
-  const res1 = pink.batchHttpRequest([{
-    url: 'https://api-mumbai.lens.dev/',
-    method: "POST",
-    headers,
-    body,
-    returnTextBody: true,
-  }]);
-  console.info(res1);
-  const res2 = pink.httpRequest({
-    url: 'https://api.chainsafe.io/api/v1/buckets',
-    method: "GET",
-    headers: headers2,
-    returnTextBody: true,
-  });
-  console.info(res2);
 
   // Convert JSON data to a Buffer
-  const jsonBuffer = JSON.stringify({
+  const kvdbUpdate = JSON.stringify({
     "txn": [
       {"set": "hello", "value": "world"}
       //{"delete": "users:email:old@example.com"}
     ]
   });
-
-  const body2 = stringToHex(jsonBuffer);
+  const body2 = stringToHex(kvdbUpdate);
   console.log(body2)
+
+  const [res1, res2] = pink.batchHttpRequest([
+      {
+        url: odds_http_endpoint,
+        method: "GET",
+        headers,
+        returnTextBody: true,
+      },
+      {
+        url: `${kvdb_http_endpoint}/hello`,
+        method: "POST",
+        headers: headers2,
+        body: body2,
+        returnTextBody: true,
+      }
+  ]);
+  console.info(res1, res2);
   const res3 = pink.httpRequest({
-    url: 'https://kvdb.io/{}/hello',
+    url: `${tg_bot_http_endpoint}${res1.body}`,
     method: "POST",
-    headers: headers2,
-    body: body2,
+    headers,
     returnTextBody: true,
   });
   console.info(res3);
